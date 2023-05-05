a43e0bf6f44d8370840e1f1c7a31266fc7462948
AntonW <ccgshaker@yandex.ru>
2023-05-05 18:09:32 +0300

    SimpleVector 1 task

--- platform/main.cpp
+++ platform/main.cpp
@@ -1,11 +1,33 @@
-#include "singlelinkedlist.h"
-#include "testsinglelinkedlist.h"
-
+#include "simple_vector.h"
+#include <iostream>
+// Tests
+#include "tests.h"
 
 int main() {
-    Test0();
     Test1();
-    Test2();
-    Test3();
-    Test4();
+    return 0;
 }
+
+// int main() {
+//     SimpleVector<int> arr(3);
+//     for (auto el : arr) std::cout << el << " "s;
+//     std::cout << std::endl;
+
+//     SimpleVector<int> arr1(3, 21);
+//     for (auto el : arr1) std::cout << el << " "s;
+//     std::cout << std::endl;
+    
+//     SimpleVector<int> arr2{1, 2, 3};
+//     for (auto el : arr2) std::cout << el << " "s;
+//     std::cout << std::endl;
+
+//     arr2.Resize(5);
+//     for (auto el : arr2) std::cout << el << " "s;
+//     std::cout << std::endl;
+
+//     arr2.Resize(1);
+//     for (auto el : arr2) std::cout << el << " "s;
+//     std::cout << std::endl;
+
+//     return 0;
+// }
