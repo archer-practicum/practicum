33ae50686faf44e05a5613f7a0ed81c58acbd12e
Work Aha <ccgshaker@yandex.ru>
2023-06-26 17:22:52 +0300

    9-13-3 begin

--- platform/main.cpp
+++ platform/main.cpp
@@ -1,203 +1,102 @@
-#include "simple_vector.h"
-
-#include <cassert>
+#include <functional>
 #include <iostream>
-#include <numeric>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <future>
+#include <execution>
 
 using namespace std;
 
-void TestReserveConstructor() {
-    cout << "TestReserveConstructor"s << endl;
-    SimpleVector<int> v(Reserve(5));
-    assert(v.GetCapacity() == 5);
-    assert(v.IsEmpty());
-    cout << "Done!"s << endl;
-}
+struct Stats {
+    map<string, int> word_frequences;
 
-void TestReserveMethod() {
-    cout << "TestReserveMethod"s << endl;
-    SimpleVector<int> v;
-    // зарезервируем 5 мест в векторе
-    v.Reserve(5);
-    assert(v.GetCapacity() == 5);
-    assert(v.IsEmpty());
-
-    // попытаемся уменьшить capacity до 1
-    v.Reserve(1);
-    // capacity должно остаться прежним
-    assert(v.GetCapacity() == 5);
-    // поместим 10 элементов в вектор
-    for (int i = 0; i < 10; ++i) {
-        v.PushBack(i);
+    void operator+=(const Stats& other) {
+        for (const auto&[word, freq] : other.word_frequences) {
+            word_frequences[word] += freq;
+        }
     }
-    assert(v.GetSize() == 10);
-    // увеличим capacity до 100
-    v.Reserve(static_cast<size_t>(100));
-    // проверим, что размер не поменялся
-    assert(v.GetSize() == 10);
-    assert(v.GetCapacity() == 100);
-    // проверим, что элементы на месте
-    for (int i = 0; i < 10; ++i) {
-        assert(v[i] == i);
-    }
-    cout << "Done!"s << endl;
-}
-
-class X {
-public:
-    X()
-        : X(5) {
-    }
-    X(size_t num)
-        : x_(num) {
-    }
-    X(const X& other) = delete;
-    X& operator=(const X& other) = delete;
-    X(X&& other) {
-        x_ = exchange(other.x_, 0);
-    }
-    X& operator=(X&& other) {
-        x_ = exchange(other.x_, 0);
-        return *this;
-    }
-    size_t GetX() const {
-        return x_;
-    }
-
-private:
-    size_t x_;
 };
 
-SimpleVector<int> GenerateVector(size_t size) {
-    SimpleVector<int> v(size);
-    iota(v.begin(), v.end(), 1);
-    return v;
+Stats operator+ (Stats const lhs, Stats const rhs) {
+    Stats tmp(lhs);
+    tmp += rhs;
+    return tmp;
 }
 
-void TestTemporaryObjConstructor() {
-    const size_t size = 1000000;
-    cout << "Test with temporary object, copy elision" << endl;
-    SimpleVector<int> moved_vector(GenerateVector(size));
-    assert(moved_vector.GetSize() == size);
-    cout << "Done!" << endl << endl;
-}
+using KeyWords = set<string, less<>>;
 
-void TestTemporaryObjOperator() {
-    const size_t size = 1000000;
-    cout << "Test with temporary object, operator=" << endl;
-    SimpleVector<int> moved_vector;
-    assert(moved_vector.GetSize() == 0);
-    moved_vector = GenerateVector(size);
-    assert(moved_vector.GetSize() == size);
-    cout << "Done!" << endl << endl;
-}
+// Stats StringToStats(const vector<string> &&words, const KeyWords& key_words) {    
+//     Stats res;
+//     for (const string &word : key_words) {
+//         res.word_frequences[word] = 0;
+//     }
+//     for_each (execution::par, words.begin(), words.end(), 
+//         [&](const string &word) { 
+//             if (key_words.count(word)) {
+//                 res.word_frequences[word] += 1;
+//             };
+//         }
+//     );
+//     return res;
+// }
 
-void TestNamedMoveConstructor() {
-    const size_t size = 1000000;
-    cout << "Test with named object, move constructor" << endl;
-    SimpleVector<int> vector_to_move(GenerateVector(size));
-    assert(vector_to_move.GetSize() == size);
-
-    SimpleVector<int> moved_vector(move(vector_to_move));
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNamedMoveOperator() {
-    const size_t size = 1000000;
-    cout << "Test with named object, operator=" << endl;
-    SimpleVector<int> vector_to_move(GenerateVector(size));
-    assert(vector_to_move.GetSize() == size);
-
-    SimpleVector<int> moved_vector = move(vector_to_move);
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableMoveConstructor() {
-    const size_t size = 5;
-    cout << "Test noncopiable object, move constructor" << endl;
-    SimpleVector<X> vector_to_move;
-    for (size_t i = 0; i < size; ++i) {
-        vector_to_move.PushBack(X(i));
+Stats StringToStats(const vector<string> &&words, const KeyWords& key_words) {    
+    Stats res;
+    for (const string word : words) {
+        if (key_words.count(word)) {
+            res.word_frequences[word] += 1;
+        }        
     }
-
-    SimpleVector<X> moved_vector = move(vector_to_move);
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-
-    for (size_t i = 0; i < size; ++i) {
-        assert(moved_vector[i].GetX() == i);
-    }
-    cout << "Done!" << endl << endl;
+    return res;
 }
 
-void TestNoncopiablePushBack() {
-    const size_t size = 5;
-    cout << "Test noncopiable push back" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
+Stats ExploreKeyWords(const KeyWords& key_words, istream& input) {
+    string word;
+    vector<string> words;
+    words.reserve(5000);
+    vector<future<Stats>> pats_stats;
+    Stats res;
 
-    assert(v.GetSize() == size);
-
-    for (size_t i = 0; i < size; ++i) {
-        assert(v[i].GetX() == i);
+    while (input >> word) {
+        if (words.size() == 5000) {
+            pats_stats.push_back(move(async(StringToStats, move(words), cref(key_words))));
+            words.reserve(5000);
+        }
+        words.push_back(move(word));
     }
-    cout << "Done!" << endl << endl;
+    pats_stats.push_back(move(async(StringToStats, move(words), cref(key_words))));
+
+    return transform_reduce(//execution::par, 
+                    pats_stats.begin(), pats_stats.end(),
+                    Stats{},
+                    [](auto &el) { return el.get(); },
+                    [](auto &l, auto &r){ return l + r; });
+    // for (auto &el : pats_stats) {
+    //     res += el.get();
+    // }
+
+    // vector<Stats> all_stats;
+    // for (auto &el : pats_stats) {
+    //     all_stats.push_back(move(el.get()));
+    // }
+    // return reduce(execution::par, all_stats.begin(), all_stats.end(), Stats{}, [](const auto &l, const auto &r) { return l + r; });
 }
-
-void TestNoncopiableInsert() {
-    const size_t size = 5;
-    cout << "Test noncopiable insert" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
-
-    // в начало
-    v.Insert(v.begin(), X(size + 1));
-    assert(v.GetSize() == size + 1);
-    assert(v.begin()->GetX() == size + 1);
-    // в конец
-    v.Insert(v.end(), X(size + 2));
-    assert(v.GetSize() == size + 2);
-    assert((v.end() - 1)->GetX() == size + 2);
-    // в середину
-    v.Insert(v.begin() + 3, X(size + 3));
-    assert(v.GetSize() == size + 3);
-    assert((v.begin() + 3)->GetX() == size + 3);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableErase() {
-    const size_t size = 3;
-    cout << "Test noncopiable erase" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
-
-    auto it = v.Erase(v.begin());
-    assert(it->GetX() == 1);
-    cout << "Done!" << endl << endl;
-}
-
 
 int main() {
-    TestReserveConstructor();
-    TestReserveMethod();
+    const KeyWords key_words = {"yangle", "rocks", "sucks", "all"};
 
-    TestTemporaryObjConstructor();
-    TestTemporaryObjOperator();
-    TestNamedMoveConstructor();
-    TestNamedMoveOperator();
-    TestNoncopiableMoveConstructor();
-    TestNoncopiablePushBack();
-    TestNoncopiableInsert();
-    TestNoncopiableErase();
+    stringstream ss;
+    ss << "this new yangle service really rocks\n";
+    ss << "It sucks when yangle isn't available\n";
+    // ss << "10 reasons why yangle is the best IT company\n";
+    // ss << "yangle rocks others suck\n";
+    // ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+
+    for (const auto& [word, frequency] : ExploreKeyWords(key_words, ss).word_frequences) {
+        cout << word << " " << frequency << endl;
+    }
+
     return 0;
 }
