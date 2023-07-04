8e0454beb66889d97ee14e26a1a05555ac2360b5
Work Aha <ccgshaker@yandex.ru>
2023-07-04 17:12:06 +0300

    10-1-5 start

--- platform/main.cpp
+++ platform/main.cpp
@@ -1,97 +1,207 @@
-#include <functional>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
-#include <map>
-#include <set>
 #include <sstream>
 #include <string>
-#include <future>
-#include <execution>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
+#include <exception>
 
 using namespace std;
 
-struct Stats {
-    map<string, int> word_frequences;
+class VehiclePlate {
+private:
+    auto AsTuple() const {
+        return tie(letters_, digits_, region_);
+    }
 
-    void operator+=(const Stats& other) {
-        for (const auto&[word, freq] : other.word_frequences) {
-            word_frequences[word] += freq;
-        }
+public:
+    bool operator==(const VehiclePlate& other) const {
+        return AsTuple() == other.AsTuple();
+    }
+
+    VehiclePlate(char l0, char l1, int digits, char l2, int region)
+        : letters_{l0, l1, l2}
+        , digits_(digits)
+        , region_(region) {
+    }
+
+    string ToString() const {
+        ostringstream out;
+        out << letters_[0] << letters_[1];
+        out << setfill('0') << right << setw(3) << digits_;
+        out << letters_[2] << setw(2) << region_;
+
+        return out.str();
+    }
+
+    size_t Hash() const {
+        return static_cast<size_t>(hash_(ToString()));
+    }
+
+private:
+    array<char, 3> letters_;
+    int digits_;
+    int region_;
+    hash<string> hash_;
+};
+
+ostream& operator<<(ostream& out, VehiclePlate plate) {
+    out << plate.ToString();
+    return out;
+}
+
+class VehiclePlateHasher {
+    public:
+    size_t operator()(const VehiclePlate &vehic_plate) const {
+        return vehic_plate.Hash();
     }
 };
 
-using KeyWords = set<string, less<>>;
+// выбросьте это исключение в случае ошибки парковки
+struct ParkingException {};
 
-vector<string> SplitIntoWords(const string &str) {
-    vector<string> res;
-    string word = "";
-    for (const char c : str) {
-        if(c != ' ') {
-            word += c;
+template <typename Clock>
+class Parking {
+    // при обращении к типу внутри шаблонного параметра мы обязаны использовать 
+    // typename; чтобы этого избежать, объявим псевдонимы для нужных типов
+    using Duration = typename Clock::duration;
+    using TimePoint = typename Clock::time_point;
+
+public:
+    Parking(int cost_per_second) : cost_per_second_(cost_per_second) {}
+
+    // запарковать машину с указанным номером
+    void Park(VehiclePlate car) {
+        if (now_parked_.count(car)) {
+            throw ParkingException();
         } else {
-            if (!word.empty()) {
-                res.push_back(move(word));
-            }
+            now_parked_[car] = TimePoint();
         }
     }
-    if (!word.empty()) {
-        res.push_back(move(word));
-    }
-    return res;
-}
 
-Stats StringToStats(const vector<string> &&strs, const KeyWords& key_words) {
-    Stats res;
-    for (const string &str : strs) {
-        for (const string word : SplitIntoWords(str)) {
-            if (key_words.count(word)) {
-                res.word_frequences[word] += 1;
-            }        
+    // забрать машину с указанным номером
+    void Withdraw(const VehiclePlate& car) {
+        if (now_parked_.count(car)) {
+            throw ParkingException();
+        } else {
+            complete_parks_[car] = Duration();
+            now_parked_.erase(car);
         }
-    }    
-    return res;
-}
-
-Stats ExploreKeyWords(const KeyWords& key_words, istream& input) {
-    string str;
-    vector<string> words;
-    words.reserve(5000);
-    vector<future<Stats>> pats_stats;
-    Stats res;
-
-    while (!input.eof()) {
-        getline(input, str);
-        if (words.size() == 5000) {
-            pats_stats.push_back(move(async(StringToStats, move(words), cref(key_words))));
-            words.reserve(5000);
-        }
-        words.push_back(move(str));
     }
-    pats_stats.push_back(move(async(StringToStats, move(words), cref(key_words))));
-    
-    for (auto &el : pats_stats) {
-        res += el.get();
+
+    // получить счёт за конкретный автомобиль
+    int64_t GetCurrentBill(const VehiclePlate& car) const {
+        // место для вашей реализации
     }
-    return res;
-}
+
+    // завершить расчётный период
+    // те машины, которые находятся на парковке на данный момент, должны 
+    // остаться на парковке, но отсчёт времени для них начинается с нуля
+    unordered_map<VehiclePlate, int64_t, VehiclePlateHasher> EndPeriodAndGetBills() {
+        // место для вашей реализации
+    }
+
+    // не меняйте этот метод
+    auto& GetNowParked() const {
+        return now_parked_;
+    }
+
+    // не меняйте этот метод
+    auto& GetCompleteParks() const {
+        return complete_parks_;
+    }
+
+private:
+    int cost_per_second_;
+    unordered_map<VehiclePlate, TimePoint, VehiclePlateHasher> now_parked_;
+    unordered_map<VehiclePlate, Duration, VehiclePlateHasher> complete_parks_;
+};
+
+// эти часы удобно использовать для тестирования
+// они покажут столько времени, сколько вы задали явно
+class TestClock {
+public:
+    using time_point = chrono::system_clock::time_point;
+    using duration = chrono::system_clock::duration;
+
+    static void SetNow(int seconds) {
+        current_time_ = seconds;
+    }
+
+    static time_point now() {
+        return start_point_ + chrono::seconds(current_time_);
+    }
+
+private:
+    inline static time_point start_point_ = chrono::system_clock::now();
+    inline static int current_time_ = 0;
+};
 
 int main() {
-    const KeyWords key_words = {"yangle", "rocks", "sucks", "all"};
+    Parking<TestClock> parking(10);
 
-    stringstream ss;
-    ss << "this new yangle service really rocks\n";
-    ss << "It sucks when yangle isn't available\n";
-    // ss << "10 reasons why yangle is the best IT company\n";
-    // ss << "yangle rocks others suck\n";
-    // ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+    TestClock::SetNow(10);
+    parking.Park({'A', 'A', 111, 'A', 99});
 
-    for (const auto& [word, frequency] : ExploreKeyWords(key_words, ss).word_frequences) {
-        cout << word << " " << frequency << endl;
+    TestClock::SetNow(20);
+    parking.Withdraw({'A', 'A', 111, 'A', 99});
+    parking.Park({'B', 'B', 222, 'B', 99});
+
+    TestClock::SetNow(40);
+    assert(parking.GetCurrentBill({'A', 'A', 111, 'A', 99}) == 100);
+    assert(parking.GetCurrentBill({'B', 'B', 222, 'B', 99}) == 200);
+    parking.Park({'A', 'A', 111, 'A', 99});
+
+    TestClock::SetNow(50);
+    assert(parking.GetCurrentBill({'A', 'A', 111, 'A', 99}) == 200);
+    assert(parking.GetCurrentBill({'B', 'B', 222, 'B', 99}) == 300);
+    assert(parking.GetCurrentBill({'C', 'C', 333, 'C', 99}) == 0);
+    parking.Withdraw({'B', 'B', 222, 'B', 99});
+
+    TestClock::SetNow(70);
+    {
+        // проверим счёт
+        auto bill = parking.EndPeriodAndGetBills();
+
+        // так как внутри макроса используется запятая,
+        // нужно заключить его аргумент в дополнительные скобки
+        assert((bill
+                == unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
+                   {{'A', 'A', 111, 'A', 99}, 400},
+                   {{'B', 'B', 222, 'B', 99}, 300},
+               }));
     }
 
-    for (const string &word : SplitIntoWords("  this new yangle service really rocks  ")) {
-        cout << word << endl;
+    TestClock::SetNow(80);
+    {
+        // проверим счёт
+        auto bill = parking.EndPeriodAndGetBills();
+
+        // так как внутри макроса используется запятая,
+        // нужно заключить его аргумент в дополнительные скобки
+        assert((bill
+                == unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
+                   {{'A', 'A', 111, 'A', 99}, 100},
+               }));
     }
 
-    return 0;
+    try {
+        parking.Park({'A', 'A', 111, 'A', 99});
+        assert(false);
+    }
+    catch (ParkingException) {
+    }
+
+    try {
+        parking.Withdraw({'B', 'B', 222, 'B', 99});
+        assert(false);
+    }
+    catch (ParkingException) {
+    }
+
+    cout << "Success!"s << endl;
 }
