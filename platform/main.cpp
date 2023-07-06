6e2219108ed2428641f92efb64ccdebec379cdba
Work Aha <ccgshaker@yandex.ru>
2023-07-06 17:22:45 +0300

    10-1-8 start

--- platform/main.cpp
+++ platform/main.cpp
@@ -1,207 +1,82 @@
-#include <array>
 #include <cassert>
-#include <chrono>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <tuple>
-#include <unordered_map>
-#include <vector>
-#include <exception>
 
-using namespace std;
-
-class VehiclePlate {
-private:
-    auto AsTuple() const {
-        return tie(letters_, digits_, region_);
-    }
-
-public:
-    bool operator==(const VehiclePlate& other) const {
-        return AsTuple() == other.AsTuple();
-    }
-
-    VehiclePlate(char l0, char l1, int digits, char l2, int region)
-        : letters_{l0, l1, l2}
-        , digits_(digits)
-        , region_(region) {
-    }
-
-    string ToString() const {
-        ostringstream out;
-        out << letters_[0] << letters_[1];
-        out << setfill('0') << right << setw(3) << digits_;
-        out << letters_[2] << setw(2) << region_;
-
-        return out.str();
-    }
-
-    size_t Hash() const {
-        return static_cast<size_t>(hash_(ToString()));
-    }
-
-private:
-    array<char, 3> letters_;
-    int digits_;
-    int region_;
-    hash<string> hash_;
+template <typename T>
+struct TreeNode {
+    T value;
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
 };
 
-ostream& operator<<(ostream& out, VehiclePlate plate) {
-    out << plate.ToString();
-    return out;
+template <typename T>
+void DeleteTree(TreeNode<T>* node) {
+    if (!node) {
+        return;
+    }
+    DeleteTree(node->left);
+    DeleteTree(node->right);
+    delete node;
 }
 
-class VehiclePlateHasher {
-    public:
-    size_t operator()(const VehiclePlate &vehic_plate) const {
-        return vehic_plate.Hash();
+template<typename T>
+bool checkSearchNode(const TreeNode<T> *node, const T *min, const T *max) {
+    if (!node->left && !node->right) return true;
+
+    if (node->left) {
+        if (node->value <= node->left->value) return false;
+        if (max) if (node->left->value >= *max) return false;
     }
-};
 
-// выбросьте это исключение в случае ошибки парковки
-struct ParkingException {};
+    if (node->right) {
+        if (!(node->value >= node->right->value)) return false;
+        if (min) if (node->right->value <= *min) return false;
+    }
 
-template <typename Clock>
-class Parking {
-    // при обращении к типу внутри шаблонного параметра мы обязаны использовать 
-    // typename; чтобы этого избежать, объявим псевдонимы для нужных типов
-    using Duration = typename Clock::duration;
-    using TimePoint = typename Clock::time_point;
+    return true;
+    
+}
 
-public:
-    Parking(int cost_per_second) : cost_per_second_(cost_per_second) {}
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node) {    
 
-    // запарковать машину с указанным номером
-    void Park(VehiclePlate car) {
-        if (now_parked_.count(car)) {
-            throw ParkingException();
+    if (node->right) {
+        if (checkSearchNode(node->right, &node->value, nullptr)) {
+            CheckTreeProperty(node->right);
         } else {
-            now_parked_[car] = TimePoint();
+            return false;
         }
     }
 
-    // забрать машину с указанным номером
-    void Withdraw(const VehiclePlate& car) {
-        if (now_parked_.count(car)) {
-            throw ParkingException();
+    if (node->left) {
+        if(checkSearchNode(node->left, nullptr, &node->value)) {
+            CheckTreeProperty(node->left);
         } else {
-            complete_parks_[car] = Duration();
-            now_parked_.erase(car);
+            return false;
         }
     }
 
-    // получить счёт за конкретный автомобиль
-    int64_t GetCurrentBill(const VehiclePlate& car) const {
-        // место для вашей реализации
-    }
+    return checkSearchNode(node, nullptr, nullptr);
 
-    // завершить расчётный период
-    // те машины, которые находятся на парковке на данный момент, должны 
-    // остаться на парковке, но отсчёт времени для них начинается с нуля
-    unordered_map<VehiclePlate, int64_t, VehiclePlateHasher> EndPeriodAndGetBills() {
-        // место для вашей реализации
-    }
-
-    // не меняйте этот метод
-    auto& GetNowParked() const {
-        return now_parked_;
-    }
-
-    // не меняйте этот метод
-    auto& GetCompleteParks() const {
-        return complete_parks_;
-    }
-
-private:
-    int cost_per_second_;
-    unordered_map<VehiclePlate, TimePoint, VehiclePlateHasher> now_parked_;
-    unordered_map<VehiclePlate, Duration, VehiclePlateHasher> complete_parks_;
-};
-
-// эти часы удобно использовать для тестирования
-// они покажут столько времени, сколько вы задали явно
-class TestClock {
-public:
-    using time_point = chrono::system_clock::time_point;
-    using duration = chrono::system_clock::duration;
-
-    static void SetNow(int seconds) {
-        current_time_ = seconds;
-    }
-
-    static time_point now() {
-        return start_point_ + chrono::seconds(current_time_);
-    }
-
-private:
-    inline static time_point start_point_ = chrono::system_clock::now();
-    inline static int current_time_ = 0;
-};
-
+}
 int main() {
-    Parking<TestClock> parking(10);
+    using T = TreeNode<int>;
+    T* root1 = new T{6, 
+        new T{4, new T{3}, new T{5}}, new T{7}};
+    assert(CheckTreeProperty(root1));
 
-    TestClock::SetNow(10);
-    parking.Park({'A', 'A', 111, 'A', 99});
+    T* root2 = new T{6, 
+        new T{4, new T{3}, new T{5}}, new T{7, new T{8}}};
+    assert(!CheckTreeProperty(root2));
 
-    TestClock::SetNow(20);
-    parking.Withdraw({'A', 'A', 111, 'A', 99});
-    parking.Park({'B', 'B', 222, 'B', 99});
+    T* root3 = new T{5,
+            new T{3, new T{1}, new T{7}}, new T{6, new T{2}, new T{8}}};
+    assert(!CheckTreeProperty(root3));
 
-    TestClock::SetNow(40);
-    assert(parking.GetCurrentBill({'A', 'A', 111, 'A', 99}) == 100);
-    assert(parking.GetCurrentBill({'B', 'B', 222, 'B', 99}) == 200);
-    parking.Park({'A', 'A', 111, 'A', 99});
+    T* root4 = new T{6, 
+            new T{4, new T{3}, new T{3}}, new T{9, new T{7}}};
+            assert(!CheckTreeProperty(root4));
 
-    TestClock::SetNow(50);
-    assert(parking.GetCurrentBill({'A', 'A', 111, 'A', 99}) == 200);
-    assert(parking.GetCurrentBill({'B', 'B', 222, 'B', 99}) == 300);
-    assert(parking.GetCurrentBill({'C', 'C', 333, 'C', 99}) == 0);
-    parking.Withdraw({'B', 'B', 222, 'B', 99});
-
-    TestClock::SetNow(70);
-    {
-        // проверим счёт
-        auto bill = parking.EndPeriodAndGetBills();
-
-        // так как внутри макроса используется запятая,
-        // нужно заключить его аргумент в дополнительные скобки
-        assert((bill
-                == unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
-                   {{'A', 'A', 111, 'A', 99}, 400},
-                   {{'B', 'B', 222, 'B', 99}, 300},
-               }));
-    }
-
-    TestClock::SetNow(80);
-    {
-        // проверим счёт
-        auto bill = parking.EndPeriodAndGetBills();
-
-        // так как внутри макроса используется запятая,
-        // нужно заключить его аргумент в дополнительные скобки
-        assert((bill
-                == unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
-                   {{'A', 'A', 111, 'A', 99}, 100},
-               }));
-    }
-
-    try {
-        parking.Park({'A', 'A', 111, 'A', 99});
-        assert(false);
-    }
-    catch (ParkingException) {
-    }
-
-    try {
-        parking.Withdraw({'B', 'B', 222, 'B', 99});
-        assert(false);
-    }
-    catch (ParkingException) {
-    }
-
-    cout << "Success!"s << endl;
+    DeleteTree(root1);
+    DeleteTree(root2);
+    DeleteTree(root3);
+    DeleteTree(root4);
 }
