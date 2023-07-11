ad7b92208f58164127f5e22289b82f7f169e9819
Work Aha <ccgshaker@yandex.ru>
2023-07-11 17:22:15 +0300

    10-2-4 begin

--- platform/main.cpp
+++ platform/main.cpp
@@ -1,82 +1,47 @@
+#include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
 
-template <typename T>
-struct TreeNode {
-    T value;
-    TreeNode* left = nullptr;
-    TreeNode* right = nullptr;
+// не меняйте файлы xml.h и xml.cpp
+#include "xml.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
 };
 
-template <typename T>
-void DeleteTree(TreeNode<T>* node) {
-    if (!node) {
-        return;
-    }
-    DeleteTree(node->left);
-    DeleteTree(node->right);
-    delete node;
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(spendings.begin(), spendings.end(), 0,
+                      [](int current, const Spending& spending) {
+                          return current + spending.amount;
+                      });
 }
 
-template<typename T>
-bool checkSearchNode(const TreeNode<T> *node, const T *min, const T *max) {
-    if (!node->left && !node->right) return true;
-
-    if (node->left) {
-        if (node->value <= node->left->value) return false;
-        if (max) if (node->left->value >= *max) return false;
-    }
-
-    if (node->right) {
-        if (!(node->value >= node->right->value)) return false;
-        if (min) if (node->right->value <= *min) return false;
-    }
-
-    return true;
-    
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
 }
 
-template <typename T>
-bool CheckTreeProperty(const TreeNode<T>* node) {    
-
-    if (node->right) {
-        if (checkSearchNode(node->right, &node->value, nullptr)) {
-            CheckTreeProperty(node->right);
-        } else {
-            return false;
-        }
-    }
-
-    if (node->left) {
-        if(checkSearchNode(node->left, nullptr, &node->value)) {
-            CheckTreeProperty(node->left);
-        } else {
-            return false;
-        }
-    }
-
-    return checkSearchNode(node, nullptr, nullptr);
-
+vector<Spending> LoadFromXml(istream& input) {
+    vector<Spending> res;
+    // Document doc = Load(input);
+    // for (const auto &node : doc.GetRoot().Children()) {
+    //     string category = node.AttributeValue<string>("category"s);
+    //     int amount = node.AttributeValue<int>("amount"s);
+    //     res.push_back({move(category), amount});
+    // }
+    return res;
 }
+
 int main() {
-    using T = TreeNode<int>;
-    T* root1 = new T{6, 
-        new T{4, new T{3}, new T{5}}, new T{7}};
-    assert(CheckTreeProperty(root1));
-
-    T* root2 = new T{6, 
-        new T{4, new T{3}, new T{5}}, new T{7, new T{8}}};
-    assert(!CheckTreeProperty(root2));
-
-    T* root3 = new T{5,
-            new T{3, new T{1}, new T{7}}, new T{6, new T{2}, new T{8}}};
-    assert(!CheckTreeProperty(root3));
-
-    T* root4 = new T{6, 
-            new T{4, new T{3}, new T{3}}, new T{9, new T{7}}};
-            assert(!CheckTreeProperty(root4));
-
-    DeleteTree(root1);
-    DeleteTree(root2);
-    DeleteTree(root3);
-    DeleteTree(root4);
+    const vector<Spending> spendings = LoadFromXml(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
 }
